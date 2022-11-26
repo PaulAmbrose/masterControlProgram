1516f05a7c65f18cc610425bc42c31924d982c2c
Paul Ambrose <pablo_rose34@hotmail.com>
2022-11-26 12:51:06 +0000

    Update pushScript.sh

--- Master Control Program/docs/Old Docs/pushScript.sh
+++ Master Control Program/docs/Old Docs/pushScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 staging_path="/home/pablorose34/Desktop/staging/activeStage"
-current_git_token="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"
+current_git_token=""
 
 git remote set-url origin https://${current_git_token}@github.com/${pablo_rose34@hotmail.com}/${https://github.com/PaulAmbrose/piSenseHat}.git
 
